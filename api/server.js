eaa507f0a695bd95dadb03196d3e110472d4f09d
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-08 10:57:39 -0400

    single functional

--- api/server.js
+++ api/server.js
@@ -15,8 +15,25 @@ server.get("/api/accounts", (req, res) => {
 });
 
 server.get("/api/accounts/:id", (req, res) => {
-  //logic to find req.params.id 404 err
-  res.send("One accout").end();
+
+  const id = req.params.id
+  db("accounts")
+    .where({ id })
+    .then((users) => {
+      if (users.length) {
+        res.status(200).json(users).end();
+      } else {
+        res.status(404).json({ message: "user not found" }).end();
+      }
+    })
+    .catch((err) =>
+      res
+        .status(500)
+        .json({
+          message: "database error",
+        })
+        .end()
+    );
 });
 
 server.post("/api/accounts", (req, res) => {
