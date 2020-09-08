e16043643d97373246741b655a2d6ade8f541dd8
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-08 10:44:42 -0400

    Argh

--- api/server.js
+++ api/server.js
@@ -6,4 +6,39 @@ const server = express();
 
 server.use(express.json());
 
+
+
+server.get('/api/accounts', (req, res)=>{
+    res.send("You got me!").end()
+  })
+  server.get('/api/accounts/:id', (req, res)=>{
+      //logic to find req.params.id 404 err
+    res.send("One accout").end()
+  })
+  
+  server.post('/api/accounts', (req, res)=>{
+    res.send("Woo").end()
+    if(req.body.name === '' || req.body.budget === ''){
+      res.status(400).json({message: "Bad Request"}).end()
+    }else{
+      res.status(500).json({message: "Back End Went Fucky"})
+    }
+  })
+  server.put('/api/accounts/:id', (req, res)=>{
+    res.send("Woo hoo").end()
+    //logic to find req.params.id 404 err
+    if(req.body.name === '' || req.body.budget === ''){
+      res.status(400).json({message: 'Nope'}).end()
+    }else{
+      res.status(500).json({message: "Back End Went Fucky"})
+    }
+  })
+  server.delete('api/accounts/:id', (req, res)=>{
+    res.send('DELETED').end()
+    //log to find req.params.id 404 err
+    //res.status 500
+  })
+
+
+
 module.exports = server;
